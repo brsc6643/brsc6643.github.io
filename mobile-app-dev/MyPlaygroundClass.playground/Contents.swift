18c2c3f12936f476a0afd208aa32f0ddc39d3074
brsc6643 <112126851+brsc6643@users.noreply.github.com>
2023-09-06 14:15:35 -0600

    updated for mobile-app-dev

---
+++ mobile-app-dev/MyPlaygroundClass.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var greeting: String  = "Hello, playground" //type declaration is optional in Swift
+
+var count: Int = 0
+
+var array = {true, false}
+var stringsArrayu = []()
+
+array.append(true)
+array.count
+array.insert(false, at: 0)
+
+//greeting = 'NewText'
+//count += 1
+
+//recall that vals are immutable while vars are mutable
+//using vals is best practice to avoid unwanted variable changes
+
+var name: String?
+
+if let url = URL(string: "https:sdfsdfsdfjf.com")
+
+print(greeting.count)
+print(count)
